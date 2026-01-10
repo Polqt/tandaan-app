c8e5ec2d2ce1e923ca49bcf1728c3aa46c28969e
Jepoy <poyhidalgo@gmail.com>
2026-01-10 20:31:16 +0800

    chore: add memoization in sidebar and devtools in the query provide.

--- components/avatars.tsx
+++ components/avatars.tsx
@@ -4,12 +4,13 @@ import { useOthers, useSelf } from "@liveblocks/react/suspense";
 import { Tooltip } from "@radix-ui/react-tooltip";
 import { TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useMemo } from "react";
 
 export default function Avatars() {
   const others = useOthers();
   const self = useSelf();
 
-  const all = [self, ...others];
+  const all = useMemo(() => [self, ...others], [self, others]);
 
   return (
     <div className="flex gap-2 items-center">
